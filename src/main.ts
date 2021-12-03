2458b87756292d5fef040784e1b5e828c74bb28b
luisvix <luisvix0@gmail.com>
2021-12-03 00:42:31 -0600

    adding autogenerated documentation and validator

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../package.json';
 
 (async () => {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder().setTitle('Finanzas personales').setVersion(version).build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3001);
 })();
