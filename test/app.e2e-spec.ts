f5fd3f5f0f515dcb13c28e841485513e1502749b
Vicente Flores <luisvix0@gmail.com>
2022-01-07 00:22:02 -0600

    e2e test updated

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { version } from '../package.json';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,7 +16,17 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+  describe('GET', () => {
+    describe('/health', () => {
+      it('should response 200', () => {
+        return request(app.getHttpServer()).get('/health').expect(200).expect('OK');
+      });
+    });
+
+    describe('/version', () => {
+      it('should response 200', () => {
+        return request(app.getHttpServer()).get('/version').expect(200).expect(version);
+      });
+    });
   });
 });
