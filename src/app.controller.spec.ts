60849d0f5938930eee388e7ec992b24e2f026e85
Vicente Flores <luisvix0@gmail.com>
2022-01-07 00:08:51 -0600

    Fixing tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { version } from '../package.json';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +10,21 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, ConfigService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('health', () => {
+    it('should return OK', () => {
+      expect(appController.getHealth()).toBe('OK');
+    });
+  });
+
+  describe('version', () => {
+    it('should return current version', () => {
+      expect(appController.getVersion()).toBe(version);
     });
   });
 });
