789eeb41c17664fa4a8a236b3bb2268df1e04a96
Vicente Flores <luisvix0@gmail.com>
2022-01-07 01:56:07 -0600

    Refactor globalConfigs

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,7 @@
 export const configServiceName = 'environmentVariables';
+
+export enum NodeEnv {
+  dev = 'development',
+  stage = 'staging',
+  prod = 'production',
+}
--- src/config/interfaces/globalConfigs.ts
+++
@@ -1,9 +0,0 @@
-export enum NodeEnv {
-  dev = 'development',
-  stage = 'staging',
-  prod = 'production',
-}
-
-export interface GlobalConfigs {
-  nodeEnv: NodeEnv | string;
-}
