8ed3cd7e306e6675fa5cfc56cdfd38dbe70a3be7
Vicente Flores <luisvix0@gmail.com>
2022-01-26 23:23:54 -0600

    Taggin for documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { GoogleIdTokenGuard } from './guards/GoogleIdTokenGuard';
 import { Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- src/balances/balances.controller.ts
+++ src/balances/balances.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { BalancesService } from './balances.service';
 
+@ApiTags('Balances')
 @Controller('balances')
 export class BalancesController {
   constructor(private readonly balancesService: BalancesService) {}
