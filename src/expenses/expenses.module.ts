8729d65faa7264378f19b8b86601c51f55b104a2
Vicente Flores <luisvix0@gmail.com>
2022-07-22 01:27:57 -0500

    Fix balance

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -3,9 +3,13 @@ import { ExpensesService } from './expenses.service';
 import { ExpensesController } from './expenses.controller';
 import { Expense, ExpenseSchema } from './schemas/expense.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Balance, BalanceSchema } from '../balances/schemas/balance.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Expense.name, schema: ExpenseSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Expense.name, schema: ExpenseSchema }]),
+    MongooseModule.forFeature([{ name: Balance.name, schema: BalanceSchema }]),
+  ],
   controllers: [ExpensesController],
   providers: [ExpensesService],
 })
--- src/incomes/incomes.module.ts
+++ src/incomes/incomes.module.ts
@@ -3,9 +3,13 @@ import { IncomesService } from './incomes.service';
 import { IncomesController } from './incomes.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Income, IncomeSchema } from './schemas/income.schema';
+import { Balance, BalanceSchema } from '../balances/schemas/balance.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Income.name, schema: IncomeSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Income.name, schema: IncomeSchema }]),
+    MongooseModule.forFeature([{ name: Balance.name, schema: BalanceSchema }]),
+  ],
   controllers: [IncomesController],
   providers: [IncomesService],
 })
