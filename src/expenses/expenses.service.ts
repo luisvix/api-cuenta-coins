2284726048ab8c8451ff2e5ddcf356bb95b94178
Vicente Flores <luisvix0@gmail.com>
2023-07-07 22:40:12 -0600

    create many expenses

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -3,8 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Balance, BalanceDocument } from '../balances/schemas/balance.schema';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
-import { createExpenseParams, findAllExpensesParams } from './interfaces/expensesService.interface';
+import {
+  createExpenseParams,
+  createManyExpensesParams,
+  findAllExpensesParams,
+} from './interfaces/expensesService.interface';
 import { Expense, ExpenseDocument } from './schemas/expense.schema';
+import { DateTime } from 'luxon';
+import { FrequencyOfMovements } from '../common/constants';
 
 @Injectable()
 export class ExpensesService {
@@ -29,6 +35,52 @@ export class ExpensesService {
     ]);
 
     return createdExpense;
+  }
+
+  async createMany({ expense, frequency, numberOfMovements }: createManyExpensesParams) {
+    const { createdBy: userId, operationDate = new Date(), amount } = expense;
+    const date = DateTime.fromJSDate(new Date(operationDate));
+    const auxAmount = amount / numberOfMovements;
+
+    let newDate;
+    const expenses = [];
+    const operations = [];
+
+    Array.from({ length: numberOfMovements }).forEach((_, index) => {
+      if (frequency === FrequencyOfMovements.monthly) {
+        newDate = date.plus({ months: index });
+      } else if (frequency === FrequencyOfMovements.weekly) {
+        newDate = date.plus({ weeks: index });
+      } else if (frequency === FrequencyOfMovements.biweekly) {
+        newDate = date.plus({ weeks: index * 2 });
+      } else if (frequency === FrequencyOfMovements.yearly) {
+        newDate = date.plus({ years: index });
+      }
+
+      expenses.push({
+        ...expense,
+        description: `${expense.description} ${index + 1} de ${numberOfMovements}`,
+        operationDate: newDate.toISO(),
+        amount: auxAmount,
+      });
+
+      operations.push({
+        updateOne: {
+          filter: {
+            userId,
+            year: newDate.year,
+            month: newDate.month,
+          },
+          update: { $inc: { expenses: auxAmount, balance: -auxAmount } },
+          upsert: true,
+        },
+      });
+    });
+
+    await Promise.all([
+      this.ExpenseModel.insertMany(expenses),
+      this.BalanceModel.bulkWrite(operations),
+    ]);
   }
 
   findAll({ userId, filter }: findAllExpensesParams) {
