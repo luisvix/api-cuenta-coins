12c396371c236e3d45ca4e217658da8c3528ffe2
Vicente Flores <luisvix0@gmail.com>
2023-07-09 12:49:27 -0600

    Fix string for query error

--- src/common/dtos/query-pagination.dto.ts
+++ src/common/dtos/query-pagination.dto.ts
@@ -1,16 +1,14 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
+import { IsNumber, IsNumberString, IsOptional, IsPositive, Min } from 'class-validator';
 
 export class QueryPaginationDto {
   @ApiPropertyOptional({ type: Number, default: 0 })
-  @IsNumber()
-  @Min(0)
   @IsOptional()
-  offset? = 0;
+  @IsNumberString()
+  offset? = '0';
 
   @ApiPropertyOptional({ type: Number, default: 10 })
-  @IsNumber()
   @IsOptional()
-  @IsPositive()
-  limit? = 10;
+  @IsNumberString()
+  limit? = '10';
 }
