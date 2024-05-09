9b00fe9712eed0b87f390c8904e1237d6de5a612
Vicente Flores <luisvix0@gmail.com>
2024-05-09 01:41:28 -0600

    remove deprecated dependencies

--- src/auth/guards/GoogleIdTokenGuard.ts
+++ src/auth/guards/GoogleIdTokenGuard.ts
@@ -1,7 +1,4 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { config } from '../../config/helpers/environmentVariables.helper';
-import { User } from '../../users/schemas/user.schema';
-import { OAuth2Client } from 'google-auth-library';
 
 @Injectable()
 export class GoogleIdTokenGuard implements CanActivate {
@@ -13,26 +10,7 @@ export class GoogleIdTokenGuard implements CanActivate {
         throw new UnauthorizedException('There is no an authorization header');
       }
 
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const [_, idToken] = req.headers.authorization.split('Bearer ');
-
-      const { clientId } = config.googleAuth;
-      const client = new OAuth2Client(clientId);
-
-      const ticket = await client.verifyIdToken({
-        idToken,
-        audience: clientId,
-      });
-
-      const { sub, email, name, picture } = ticket.getPayload();
-
-      const user: User = {
-        providerId: sub,
-        email: email,
-        name: name,
-        picture: picture,
-      };
-      req.user = user;
+      // implement logic to validate token
       return true;
     } catch (error) {
       throw new UnauthorizedException('Invalid token');
